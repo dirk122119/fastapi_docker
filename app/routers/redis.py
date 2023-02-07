3a51db62707efab49c28403453983beb80273459
dirk122119 <dirk122119@gmail.com>
2023-02-07 14:36:09 +0800

    fix redis return

--- app/routers/redis.py
+++ app/routers/redis.py
@@ -3,6 +3,5 @@ from fastapi import APIRouter,Request
 router=APIRouter()
 @router.get("/redis", tags=["redis"])
 async def redis_index(request:Request):
-    value= await request.app.state.redis.json().get("realtime_data")
-    print(value)
-    return {"get":value}
+    value= await request.app.state.redis.json().get("realtime:data")
+    return value
