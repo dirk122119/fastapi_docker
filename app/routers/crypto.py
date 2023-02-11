dc35d5a45d81ef0e9e35f23e267377d7e7402a08
dirk122119 <dirk122119@gmail.com>
2023-02-11 17:52:54 +0800

    fix crypto router function name

--- app/routers/crypto.py
+++ app/routers/crypto.py
@@ -15,13 +15,13 @@ def crypto_index():
     return {"crypto index"}
 
 @router.get("/crypto/top7_search", tags=["crypto"])
-async def top7_search():
+async def get_coingecko_top7_search():
     res = requests.get('https://api.coingecko.com/api/v3/search/trending')
 
     return res.json()
 
 @router.get("/crypto/sparkline/{coin}",tags=["crypto"])
-async def dashboard_data(coin):
+async def get_1d_sparkline(coin):
     chart = requests.get(f'https://api.coingecko.com/api/v3/coins/{coin}/market_chart?vs_currency=usd&days=1')
 
     data=functools.reduce(operator.iconcat, chart.json()["prices"], [])
@@ -35,8 +35,15 @@ async def dashboard_data(coin):
     logger.info("get api")
     return {"time_series_24h":time_series_24h,"prices_series_24h":prices_series_24h,"chanhePrice_24h":prices_series_24h[-1]-prices_series_24h[0]}
 
+@router.get("/crypto/top250_market_now",tags=["crypto"])
+async def get_coinlist_top250_market_now():
+    url=f"https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=true&price_change_percentage=1h%2C24h%2C7d"
+    data=requests.get(url)
+
+    return data.json()
+
 @router.get("/crypto/market_now/{coinlist}",tags=["crypto"])
-async def market_now(coinlist):
+async def get_coinlist_market_now(coinlist):
     url=f"https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids={coinlist}&order=market_cap_desc&per_page=100&page=1&sparkline=true&price_change_percentage=1h,24h,7d"
     market_list=requests.get(url)
     for coin in market_list.json():
