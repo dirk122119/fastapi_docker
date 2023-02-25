fbafbe241f21cb3de61a5bcfadd469dec3e62ad1
dirk122119 <dirk122119@gmail.com>
2023-02-25 20:22:03 +0800

    add redis for realtime us tw crypto

--- app/routers/redis.py
+++ app/routers/redis.py
@@ -1,12 +1,27 @@
 from fastapi import APIRouter,Request
 
 router=APIRouter()
-@router.get("/redis", tags=["redis"])
+@router.get("/redis_dashboard", tags=["redis"])
 async def redis_index(request:Request):
     value= await request.app.state.redis.json().get("realtime:dashboard")
     return value
 
 @router.get("/redis_tw", tags=["redis"])
 async def redis_index(request:Request):
-    value= await request.app.state.redis.json().get("realtime:tw")
+    value= await request.app.state.redis.json().get("realtime:TW_realtime")
+    return value
+
+@router.get("/redis_us", tags=["redis"])
+async def redis_index(request:Request):
+    value= await request.app.state.redis.json().get("realtime:US_daily")
+    return value
+
+@router.get("/redis_crypto", tags=["redis"])
+async def redis_index(request:Request):
+    value= await request.app.state.redis.json().get("realtime:crypto")
+    return value
+
+@router.get("/redis_coingecko_top7", tags=["redis"])
+async def redis_index(request:Request):
+    value= await request.app.state.redis.json().get("realtime:crypto_top7")
     return value
