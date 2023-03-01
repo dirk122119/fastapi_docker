5a7766f2d9ed3c7f75b78f22ae33a7aaa3a8c1b7
dirk122119 <dirk122119@gmail.com>
2023-03-01 13:59:05 +0800

    add crypto symbol

--- app/routers/redis.py
+++ app/routers/redis.py
@@ -25,3 +25,8 @@ async def redis_crypto_realtime(request:Request):
 async def redis_crypto_coingecko_top7(request:Request):
     value= await request.app.state.redis.json().get("realtime:crypto_top7")
     return value
+
+@router.get("/redis_crypto_symbol", tags=["redis"])
+async def redis_crypto_realtime(request:Request):
+    value= await request.app.state.redis.json().get("realtime:crypto_symbol")
+    return value
