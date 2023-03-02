9b0802386dafdf48a14bf61c5f904586e48dfbd3
dirk122119 <dirk122119@gmail.com>
2023-03-02 14:07:06 +0800

    add filiter game is finish

--- app/routers/betit.py
+++ app/routers/betit.py
@@ -54,14 +54,15 @@ async def get_game():
 
     connect_objt=cnx.get_connection()
     cursor = connect_objt.cursor()
-    sql="select market,symbol,date,price,direct,createrId from GameTable;"
+    sql="select market,symbol,date,price,direct,createrId,isFininsh,isReach from GameTable;"
     cursor.execute(sql)
     data=cursor.fetchall()
     data_list=[]
     for row in data:
-        sql="select name from UserTable where id=%s;"
-        value=(row[5],)
-        cursor.execute(sql,value)
-        user=cursor.fetchone()
-        data_list.append({"market":row[0],"symbol":row[1],"date":str(row[2]),"price":row[3],"direct":row[4],"creater":user[0]})
+        if(row[6]):
+            sql="select name from UserTable where id=%s;"
+            value=(row[5],)
+            cursor.execute(sql,value)
+            user=cursor.fetchone()
+            data_list.append({"market":row[0],"symbol":row[1],"date":str(row[2]),"price":row[3],"direct":row[4],"creater":user[0]})
     return JSONResponse(status_code=200, content={"data":data_list})
